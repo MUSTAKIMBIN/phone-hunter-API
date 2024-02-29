325ead67e4bfbd9f71e23c617df02ed1e4032b94
MUSTAKIMBIN <mustakimbin24@gmail.com>
2024-02-29 15:21:17 +0600

    added module functionality

--- js/phone.js
+++ js/phone.js
@@ -18,7 +18,7 @@ const displayPhone = (phones) => {
   }
   phones = phones.slice(0, 12);
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const phoneCard = document.createElement("div");
     phoneCard.classList.add("card", "p-5", "bg-gray-100", "shadow-xl");
     phoneCard.innerHTML = ` 
@@ -27,28 +27,54 @@ const displayPhone = (phones) => {
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>${phone.slug}</p>
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick="showDetels('${phone.slug}')" class="btn btn-primary">Show Detels</button>
           </div>
         </div>
         `;
     phoneContainer.appendChild(phoneCard);
   });
-  loddingSpinner(false)
+  loddingSpinner(false);
 };
 // handel serch
 const serchHandel = () => {
-    loddingSpinner(true)
+  loddingSpinner(true);
   const serchFeld = document.getElementById("serch-feld");
   const serchFeldText = serchFeld.value;
   lodePhone(serchFeldText);
 };
 const loddingSpinner = (isLodding) => {
-    const loddingSpinner = document.getElementById("loading-spinner")
-    if(isLodding){
-        loddingSpinner.classList.remove("hidden")
-    }else{
-        loddingSpinner.classList.add("hidden")
-    }
-}
+  const loddingSpinner = document.getElementById("loading-spinner");
+  if (isLodding) {
+    loddingSpinner.classList.remove("hidden");
+  } else {
+    loddingSpinner.classList.add("hidden");
+  }
+};
 
-lodePhone();
+const showDetels = async (id) => {
+  const res = await fetch(
+    ` https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  const phone = data.data
+  
+  showPhoneDetels(phone)
+};
+const showPhoneDetels = (phone)=>{
+    const showDetelsContainer = document.getElementById("show-detel-container")
+    showDetelsContainer.innerHTML= `
+    <img src="${phone.image}" alt="">
+    <h2 class="text-3xl font-bold my-2">${phone.name}</h2>
+    <p><span class="text-xl font-bold">Storage : </span>${phone.mainFeatures.storage}</p>
+    <p><span class="text-xl font-bold">Display Size : </span>${phone.mainFeatures.displaySize}</p>
+    <p><span class="text-xl font-bold">Chipset : </span>${phone.mainFeatures.chipSet}</p>
+    <p><span class="text-xl font-bold">Memory : </span>${phone.mainFeatures.memory}</p>
+    <p><span class="text-xl font-bold">Slug : </span>${phone.slug}</p>
+    <p><span class="text-xl font-bold">Release data : </span>${phone.releaseDate}</p>
+    <p><span class="text-xl font-bold">Brand : </span>${phone.brand}</p>
+    <p><span class="text-xl font-bold">GPS : </span>${phone.others.GPS}</p>
+    `
+    console.log(phone)
+    show_detel_modal.showModal(phone)
+}
+// lodePhone();
